9aca3c10f62458cfd795ecac36dfc9d54de1fc4d
sneeu <john@sneeu.com>
2012-01-04 16:25:28 +0000

    Adds an Nginx config file, and deploy commands to Fabric.

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,19 @@
-from stationary import build, clean, serve
+from fabric.api import local, run
+import stationary
+
+
+def clean():
+    stationary.clean()
+
+
+def build():
+    stationary.build()
+
+
+def serve():
+    stationary.serve()
+
+
+def deploy():
+    local('git add -p && git commit && git push')
+    run('git pull')
